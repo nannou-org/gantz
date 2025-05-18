683883fec5baa17ec6ee3cffc33fe8147ea532cd
mitchmindtree <mail@mitchellnordine.com>
2025-05-18 21:46:16 +1000

    feat: Implement proper logging in Log node

--- crates/gantz_std/src/log.rs
+++ crates/gantz_std/src/log.rs
@@ -1,5 +1,9 @@
-use gantz_core::steel::{parser::ast::ExprKind, steel_vm::engine::Engine};
+use gantz_core::steel::{
+    parser::ast::ExprKind,
+    steel_vm::{engine::Engine, register_fn::RegisterFn},
+};
 use serde::{Deserialize, Serialize};
+use steel::SteelVal;
 
 /// A simple node that logs whatever value is received at a given log level.
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Deserialize, Serialize)]
@@ -24,7 +28,7 @@ impl gantz_core::Node for Log {
         let Some(Some(input)) = inputs.get(0) else {
             return ExprKind::empty();
         };
-        let _level = match self.level {
+        let level = match self.level {
             log::Level::Error => "error",
             log::Level::Warn => "warn",
             log::Level::Info => "info",
@@ -32,8 +36,31 @@ impl gantz_core::Node for Log {
             log::Level::Trace => "trace",
         };
         // TODO: Switch to proper logging. Reference steel logging.scm example.
-        let expr = format!("(displayln {input})");
+        let expr = format!("(log/{level} {input})");
         Engine::emit_ast(&expr).unwrap().into_iter().next().unwrap()
+    }
+
+    fn register(&self, _path: &[gantz_core::node::Id], vm: &mut Engine) {
+        fn error(val: SteelVal) {
+            log::error!("{}", val);
+        }
+        fn warn(val: SteelVal) {
+            log::warn!("{}", val);
+        }
+        fn info(val: SteelVal) {
+            log::info!("{}", val);
+        }
+        fn debug(val: SteelVal) {
+            log::debug!("{}", val);
+        }
+        fn trace(val: SteelVal) {
+            log::trace!("{}", val);
+        }
+        vm.register_fn("log/error", error);
+        vm.register_fn("log/warn", warn);
+        vm.register_fn("log/info", info);
+        vm.register_fn("log/debug", debug);
+        vm.register_fn("log/trace", trace);
     }
 }
 
