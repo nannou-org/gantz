45eae7fb69a778c9082f45870afbd9760f28f83d
mitchmindtree <mail@mitchellnordine.com>
2025-05-19 12:41:24 +1000

    feat: Add basic steel_view widget

--- crates/gantz_egui/src/widget.rs
+++ crates/gantz_egui/src/widget.rs
@@ -1,3 +1,12 @@
 //! A collection of useful widgets for gantz.
 
+pub use log_view::LogView;
+
 pub mod log_view;
+
+/// Simple shorthand for viewing steel code.
+pub fn steel_view(ui: &mut egui::Ui, code: &str) {
+    let language = "scm";
+    let theme = egui_extras::syntax_highlighting::CodeTheme::from_memory(ui.ctx(), ui.style());
+    egui_extras::syntax_highlighting::code_view_ui(ui, &theme, code, language);
+}
