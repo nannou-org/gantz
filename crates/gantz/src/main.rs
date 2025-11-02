e7bad804a8abbf97c709528272194b0702c72421
mitchmindtree <mail@mitchellnordine.com>
2025-11-03 10:44:09 +1100

    feat: Add missing required core pipeline feature, fix fit_to_canvas behaviour

--- crates/gantz/src/main.rs
+++ crates/gantz/src/main.rs
@@ -1,13 +1,28 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::{Window, WindowPlugin},
+};
 use bevy_egui::{EguiContexts, EguiPlugin, EguiPrimaryContextPass, egui};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(window_plugin()))
         .add_plugins(EguiPlugin::default())
         .add_systems(Startup, setup_camera)
         .add_systems(EguiPrimaryContextPass, gui)
         .run();
+}
+
+fn window_plugin() -> WindowPlugin {
+    WindowPlugin {
+        primary_window: Some(Window {
+            title: "gantz".into(),
+            name: Some("gantz".into()),
+            fit_canvas_to_parent: true,
+            ..default()
+        }),
+        ..default()
+    }
 }
 
 fn setup_camera(mut commands: Commands) {
@@ -18,8 +33,7 @@ fn gui(mut ctxs: EguiContexts) -> Result {
     egui::containers::CentralPanel::default()
         .frame(egui::Frame::default())
         .show(ctxs.ctx_mut()?, |_ui| {
-            // TODO: Gantz widget.
+            // TODO
         });
-
     Ok(())
 }
