4530826cf28e3d6725a4a90a2682954a1c2a8477
mitchmindtree <mitchmindtree@users.noreply.github.com>
2026-01-25 12:28:01 +0000

    deploy: bc75d0be6dad5eff6ff1e77be21f8868972d6247

---
+++ pr-preview/pr-160/gantz_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wasm_bindgen_func_elem_19757: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_79846: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_79847: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_19192: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_67491: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_76565: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
